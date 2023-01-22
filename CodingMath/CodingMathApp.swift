07f1d019c7abff59dd46f0c1e90a08cbdc906c5f
Mahdi Mahjoobi <mahdi.mahjoobi@impalastudios.com>
2023-01-22 16:36:37 +0100

    Initial Commit

---
+++ CodingMath/CodingMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingMathApp.swift
+//  CodingMath
+//
+//  Created by Mahdi Mahjoobi on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CodingMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
